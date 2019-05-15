94fa3342ec167f65b076a42b0c90b34b0ce3a36f
chaithra <chaitragshetty>
2019-05-15 19:01:49 +0530

    sprinngxmldemo

---
+++ src/main/java/com/stackroute/beans/Movie.java
@@ -0,0 +1,30 @@
+package com.stackroute.beans;
+
+public class Movie {
+    Actor actor;
+
+    Movie() {
+
+    }
+
+    Movie(Actor actor) {
+        this.actor = actor;
+    }
+
+    public Actor getActor() {
+        return actor;
+    }
+
+    public void setActor(Actor actor) {
+        this.actor = actor;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "actor=" + actor +
+                '}';
+    }
+}
+
+
